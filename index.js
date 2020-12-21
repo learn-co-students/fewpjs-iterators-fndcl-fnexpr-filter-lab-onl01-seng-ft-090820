4b21948c5fde6a738ba4015f87950ae9e35d0361
jwatts82 <jessperr182@hotmail.com>
2020-12-20 22:54:52 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+
+function findMatching(array, string) {
+     return array.filter(n => {
+         return n.toLowerCase() === string.toLowerCase()
+     })
+
+}
+
+function fuzzyMatch(array, str) { 
+    return array.filter( driverMatch => { 
+        return driverMatch.startsWith(str)
+    })
+}
+
+
+function matchName(array, string) {
+    let matchingElement = array.filter(object => {
+        return object.name === string})
+    return matchingElement
+
+
+
+}
