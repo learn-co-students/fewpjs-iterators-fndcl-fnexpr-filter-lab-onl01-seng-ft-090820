19e8c9c77b99268efef078297e79522d47cbf220
cailinkless <cailin.nysff@gmail.com>
2020-12-22 13:50:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution here
+
+function findMatching(drivers, string) {
+    let matching = drivers.filter( x => {
+        return x.toLowerCase() === string.toLowerCase()
+    })
+    if (!!matching === true) {
+        return matching
+    }
+}
+
+// let even = arr.filter(n => {
+//     return n % 2 === 0;
+//   });
+  // even = [2,4,6]
+
+function fuzzyMatch(drivers, string) {
+    let similar = drivers.filter( x => {
+        return x.toLowerCase()[0] === string.toLowerCase()[0]
+    })
+    if (!!similar === true) {
+        return similar
+    }
+}
+
+function matchName(drivers, string) {
+    let match = drivers.filter( x => { 
+        return x.name === string
+    })
+    if (!!match == true) {
+      return match
+    }
+}
